3b678239e5964990628e59c751c95234534c2a82
Luis Zepeda <zepedagonzalezluis@gmail.com>
2026-01-28 19:46:18 -0600

    feat: Pausa y reanudacion de consumo de mensajes

--- src/main/java/com/pinncode/kafka/KafkaServiceApplication.java
+++ src/main/java/com/pinncode/kafka/KafkaServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.List;
@@ -18,9 +19,13 @@ public class KafkaServiceApplication implements CommandLineRunner {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    private KafkaListenerEndpointRegistry registry;
+
     private static final Logger log = LoggerFactory.getLogger(KafkaServiceApplication.class);
 
-    @KafkaListener(topics = "pinncode-topic", containerFactory = "listerContainerFactory", groupId = "pinncode-group",
+    @KafkaListener(id = "pinncodeId", autoStartup = "false",
+            topics = "pinncode-topic", containerFactory = "listerContainerFactory", groupId = "pinncode-group",
     properties = {
             "max.poll.interval.ms:4000", "max.poll.records:10"
     })
@@ -36,9 +41,15 @@ public class KafkaServiceApplication implements CommandLineRunner {
 	}
 
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws InterruptedException {
         for (int i = 0; i < 100; i++) {
             kafkaTemplate.send("pinncode-topic", String.valueOf(i), String.format("Este es un nuevo mensaje %d", i));
         }
+        log.info("Esperando para iniciar");
+        Thread.sleep(5000);
+        log.info("Starting");
+        registry.getListenerContainer("pinncodeId").start();
+        Thread.sleep(5000);
+        registry.getListenerContainer("pinncodeId").stop();
     }
 }
