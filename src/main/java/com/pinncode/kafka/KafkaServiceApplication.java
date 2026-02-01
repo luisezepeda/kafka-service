4724635e8664ccb5d545516b088172ac30a8735b
Luis Zepeda <zepedagonzalezluis@gmail.com>
2026-01-31 18:05:08 -0600

    feat: accedo a metricas

--- src/main/java/com/pinncode/kafka/KafkaServiceApplication.java
+++ src/main/java/com/pinncode/kafka/KafkaServiceApplication.java
@@ -1,10 +1,10 @@
 package com.pinncode.kafka;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,17 +14,20 @@ import org.springframework.scheduling.annotation.Scheduled;
 import java.util.List;
 
 @SpringBootApplication
-public class KafkaServiceApplication implements CommandLineRunner {
+public class KafkaServiceApplication {
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     private static final Logger log = LoggerFactory.getLogger(KafkaServiceApplication.class);
 
-    @KafkaListener(id = "pinncodeId", autoStartup = "false",
+    @KafkaListener(id = "pinncodeId", autoStartup = "true",
             topics = "pinncode-topic", containerFactory = "listerContainerFactory", groupId = "pinncode-group",
     properties = {
-            "max.poll.interval.ms:4000", "max.poll.records:10"
+            "max.poll.interval.ms:4000", "max.poll.records:50"
     })
     public void listen (List<ConsumerRecord<String, String>> messages) {
         for(ConsumerRecord<String, String> message : messages) {
@@ -37,15 +40,16 @@ public class KafkaServiceApplication implements CommandLineRunner {
 		SpringApplication.run(KafkaServiceApplication.class, args);
 	}
 
-    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    @Scheduled(fixedDelay = 2000, initialDelay = 500)
     public void print () {
-        log.info("Pinncode rocks");
-    }
-
-    @Override
-    public void run(String... args) throws InterruptedException {
         for (int i = 0; i < 100; i++) {
             kafkaTemplate.send("pinncode-topic", String.valueOf(i), String.format("Este es un nuevo mensaje %d", i));
         }
+    }
+
+    @Scheduled(fixedDelay = 2000, initialDelay = 1000)
+    public void printMetrics () {
+        double count = meterRegistry.get("kafka.producer.record.send.total").functionCounter().count();
+        log.info("Count {}", count);
     }
 }
